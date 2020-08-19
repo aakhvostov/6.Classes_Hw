ec811fa706919cdf53fc4d62bbaadbf162f9557f
Khvostov Andrey <skitol@yandex.ru>
2020-08-19 16:04:15 +0300

    6.classes 1st iteration adeed

---
+++ 6.Classes.py
@@ -0,0 +1,62 @@
+class main:
+    deliver_value = 0
+    graze = False
+    feed = ''
+    voice = ''
+    deliver_value = 0
+    def walk(self):
+        graze = True
+        print(f'скушали {feed}')
+
+    def voice(self):
+        print(f'кричит {voice}')
+
+
+
+
+class birds:
+    wings = True
+    delivery = 'egg'
+
+    def take_deliver(self):
+        if graze == True:
+            deliver_value = random.randint(1, 5)
+            print(deliver_value)
+class goose:
+    feed = 'roots'
+    voice = 'ga-ga-ga'
+
+class chicken:
+    feed = 'millet'
+    voice = 'ko-ko-ko'
+
+class duck:
+    feed = 'corn'
+    voice = 'krya-krya'
+
+class animals:
+    horns = True
+    feed = 'hay'
+
+    def take_deliver(self):
+        if graze == True:
+            deliver_value = random.uniform(1.0, 10.0)
+            print(deliver_value)
+
+class cow:
+    name = 'Manya'
+    delivery = 'milk'
+    voice = 'moooo'
+
+class goat:
+    delivery = 'milk'
+    voice = 'bee'
+
+class sheep:
+    delivery = 'wool'
+    voice = 'bkheee'
+    species #(порода)
+
+
+
+
