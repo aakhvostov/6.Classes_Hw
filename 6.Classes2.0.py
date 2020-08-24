75981866878e4ab1c9293eb4ad32af747c04fbea
Khvostov Andrey <skitol@yandex.ru>
2020-08-24 16:23:01 +0300

    Hw 6.Classes v3.0

--- 6.Classes2.0.py
+++ 6.Classes2.0.py
@@ -1,15 +1,15 @@
 import random
-random.randint(1, 40)
-random.uniform(1.0, 19.9)
+
 
 class Creature:
     dict_of_creatures = {}
+    dict_of_weight = {}
     noise = ''
 
     def __init__(self, name, weight):
         self.name = name
         self.weight = weight
-        Creature.dict_of_creatures[self.name] = self.weight
+        Creature.dict_of_weight[self.name] = self.weight
 
     def go_to_walk(self):
         print(f'{self.name} had walked')
@@ -19,9 +19,6 @@ class Creature:
 
     def feed(self):
         print(f'{self.name} eating - "om-nom-nom!"')
-
-    # def get_dict_of_creatures(self):
-    #     Creature.dict_of_creatures[self.name] = self.weight
 
 
 class Bird(Creature):
@@ -81,49 +78,44 @@ class Duck(Bird):
     creature_type = 'Утка'
     noise = 'Кря-кря-кря'
 
-
-# dori = Duck('Dori', 4)
-# dori.get_dict_of_creatures()
-# dori.feed()
-# print(Creature.__dict__)
-# kiri = Goose('Kiri', 15, 'White')
-# kiri.get_dict_of_creatures()
-# kiri.feed()
-# print(f'{kiri.name} is {kiri.color}')
-# print(Creature.__dict__)
-# koko = Chicken('Sel', 2, 'Kykareky')
-# koko.get_dict_of_creatures()
-# koko.make_a_noise()
-# koko.feed()
-# print(Creature.dict_of_creatures)
-
-
-def get_creatures():
-    cow = Cow('Маня', random.randint(20, 50))
-    goat1 = Goat('Варечка', random.randint(12, 19), 'рога')
-    goat2 = Goat('Cарочка', random.randint(12, 19), 'копыта')
-    sheep1 = Sheep('Маруся', random.randint(15, 30), 'бараш')
-    sheep2 = Sheep('Алиса', random.randint(15, 30), 'кудряш')
-    goose1 = Goose('Саня', random.randint(9, 17), 'белый')
-    goose2 = Goose('Даня', random.randint(9, 17), 'серый')
-    chick1 = Chicken('Кокошка', round(random.uniform(1.0, 9.9), 2), 'Ко-ко-ко')
-    chick2 = Chicken('Кукарешка', round(random.uniform(1.0, 9.9), 2), 'Ку-ка-реку')
-    duck = Duck('Донни', random.randint(10, 23))
-
-
-get_creatures()
+list_of_creatures = []
+cow = Cow('Буренка', random.randint(20, 50))
+list_of_creatures.append(cow)
+goat1 = Goat('Варечка', random.randint(12, 23), 'рога')
+list_of_creatures.append(goat1)
+goat2 = Goat('Маня', random.randint(12, 23), 'копыта')
+list_of_creatures.append(goat2)
+sheep1 = Sheep('Маруся', random.randint(15, 30), 'бараш')
+list_of_creatures.append(sheep1)
+sheep2 = Sheep('Алиса', random.randint(15, 30), 'кудряш')
+list_of_creatures.append(sheep2)
+goose1 = Goose('Саня', random.randint(9, 17), 'белый')
+list_of_creatures.append(goose1)
+goose2 = Goose('Даня', random.randint(9, 17), 'серый')
+list_of_creatures.append(goose2)
+chick1 = Chicken('Кокошка', round(random.uniform(1.0, 9.9), 2), 'Ко-ко-ко')
+list_of_creatures.append(chick1)
+chick2 = Chicken('Кукарешка', round(random.uniform(1.0, 9.9), 2), 'Ку-ка-реку')
+list_of_creatures.append(chick2)
+duck = Duck('Донни', random.randint(10, 23))
+list_of_creatures.append(duck)
 
 
 def get_max_weight():
-    print(f'Максимальный вес у {max(Creature.dict_of_creatures, key=Creature.dict_of_creatures.get)} - '
-          f'{max(Creature.dict_of_creatures.values())} кг')
+    print(f'Максимальный вес у {max(Creature.dict_of_weight, key=Creature.dict_of_weight.get)} - '
+          f'{max(Creature.dict_of_weight.values())} кг')
+
+
+def get_sum_of_weight():
+    print(f'общий вес всех созданий - {sum(Creature.dict_of_weight.values())} кг')
 
 
 get_max_weight()
+get_sum_of_weight()
 
 
-def get_summ_of_weight():
-    print(f'общий вес всех созданий - {sum(Creature.dict_of_creatures.values())} кг')
-
-
-get_summ_of_weight()
+for creature in list_of_creatures:
+    print()
+    creature.go_to_walk()
+    creature.feed()
+    creature.make_a_noise()
